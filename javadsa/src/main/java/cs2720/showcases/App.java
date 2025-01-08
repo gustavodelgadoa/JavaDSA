78906f4634c2beddcfccf48bc7de5a3d7ed0550c
Gustavo Delgado <gustavodelgadoa@outlook.com>
2025-01-08 18:41:54 -0500

    Initial commit

---
+++ javadsa/src/main/java/cs2720/showcases/App.java
@@ -0,0 +1,14 @@
+package cs2720.showcases;
+
+/**
+ * This file marks the first file created for this repo. 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Universe!" );
+        System.out.println("This is the second line. ");
+    }
+}
