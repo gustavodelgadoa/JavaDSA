061d1150f694ad8d2511c8f257f6cf5e2211fbb3
Gustavo Delgado <gustavodelgadoa@outlook.com>
2025-01-09 14:12:11 -0500

    testing commit + pushes.

--- javadsa/src/main/java/cs2720/showcases/App.java
+++ javadsa/src/main/java/cs2720/showcases/App.java
@@ -1,14 +1,29 @@
 package cs2720.showcases;
 
 /**
- * This file marks the first file created for this repo. 
+ * This file marks the first file created for this repo.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Universe!" );
+public class App {
+    private int count;
+
+    public static void main(String[] args) {
+        System.out.println("Hello Universe!");
         System.out.println("This is the second line. ");
     }
+
+    /**
+     * Constructor example.
+     */
+    public void counter() {
+        count = 0;
+    }
+
+    /**
+     * Accessor method example. Returns count.
+     */
+    public int getCount() {
+        return count;
+    }
+
 }
