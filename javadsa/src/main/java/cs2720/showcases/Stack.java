f69db2d87af2a94bde453d6c379d6e307e01b08c
Gustavo Delgado <gustavodelgadoa@outlook.com>
2025-01-28 15:56:57 -0500

    Completed javadoc comments for Stack interface.

--- javadsa/src/main/java/cs2720/showcases/Stack.java
+++ javadsa/src/main/java/cs2720/showcases/Stack.java
@@ -1,38 +1,43 @@
 package cs2720.showcases;
 
 /**
- * A collection of objects that are inserted and removed according to the 
+ * A collection of objects that are inserted and removed according to the
  * last-in first-out principle. Interface differs from java.util.stack.
  */
 public interface Stack<E> {
-    
+
     /**
      * Returns the number of elements in the stack.
-     * @return
+     * 
+     * @return number of elements in the stack.
      */
     int size();
-    
+
     /**
      * Tests whether the stack is empty.
-     * @return
+     * 
+     * @return true if the stack is empty, false otherwise.
      */
     boolean isEmpty();
-    
+
     /**
      * Inserts an element at the top of the stack.
-     * @return
+     * 
+     * @return e the element to be inserted.
      */
     void push(E e);
-    
+
     /**
      * Returns, but does not remove, the element at the top of the stack.
-     * @return
+     * 
+     * @return top element in the stack (null if empty)
      */
     E top();
 
     /**
      * Removes and returns the top element from the stack.
-     * @return
+     * 
+     * @return element removed (null if empty)
      */
-    E pop();    
+    E pop();
 } // Stack
