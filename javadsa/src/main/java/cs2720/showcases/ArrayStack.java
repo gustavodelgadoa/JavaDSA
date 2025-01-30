7b3d2d5d39d6118107c08b6329321e72aa20d488
Gustavo Delgado <gustavodelgadoa@outlook.com>
2025-01-29 19:09:23 -0500

    Added Array-based Stack class

---
+++ javadsa/src/main/java/cs2720/showcases/ArrayStack.java
@@ -0,0 +1,47 @@
+package cs2720.showcases;
+
+public class ArrayStack<E> implements Stack<E> { 
+    public static final int CAPACITY = 1000;
+    private E[] data;
+    private int t = -1;
+    
+    public ArrayStack() {
+        this(CAPACITY);
+    } // ArrayStack
+
+    public ArrayStack(int CAPACITY) { 
+        data = (E[]) new Object[CAPACITY];
+    } // ArrayStack
+
+    public int size() {
+        return (t+1);
+    } // size
+
+    public boolean isEmpty() {
+        return (t==-1);
+    } // isEmpty
+
+    public void push(E e) throws IllegalStateException { 
+        if (size() == data.length) { 
+            throw new IllegalStateException("Stack is full");
+        } // if
+        data[++t] = e;
+    } // push
+
+    public E top() { 
+        if (isEmpty()) { 
+            return null;
+        } // if
+        return data[t];
+    } // top
+
+    public E pop() { 
+        if (isEmpty()) { 
+            return null;
+        } // if
+        E answer = data[t];
+        data[t] = null;
+        t--;
+        return answer;
+    } // pop
+} // ArrayStack
